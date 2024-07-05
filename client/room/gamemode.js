20f4c061c1749c8b560af2b0e24e7a05efbe4045
Zttp <162319093+Zttp@users.noreply.github.com>
2024-07-05 14:02:42 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -13,7 +13,7 @@ import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, B
 const GRADIENT = API.GameMode.Parameters.GetBool("gradient"),APMIN = "FCB44B3BFF4A9878", ADMIN = "D411BD94CAE31F89", BANNED = "9D481006E2EC6AD", COLORS = [ColorsLib.ColorToHex(ColorsLib.Colors.Red), ColorsLib.ColorToHex(ColorsLib.Colors.Blue), ColorsLib.ColorToHex(ColorsLib.Colors.Lime), ColorsLib.ColorToHex(ColorsLib.Colors.Yellow), ColorsLib.ColorToHex(ColorsLib.Colors.Cyan), ColorsLib.ColorToHex(ColorsLib.Colors.Magenta), ColorsLib.ColorToHex(ColorsLib.Colors.Purple), ColorsLib.ColorToHex(ColorsLib.Colors.White)];
 // Доступ к функциям и модулям из "терминала"
 globalThis.API = API;
-globalThis.Никнейм = Никнейм;
+globalThis.Nick = Nick;
 globalThis.Статус = Статус;
 globalThis.Бан = Бан;
 globalThis.Адм = Адм;
@@ -294,7 +294,7 @@ function Статус(id,status) {
         p.PopUp(`Вам присвоен статус "${status}" !`);
     }
 }
-function Никнейм(id) {
+function Nick(id) {
     let player = API.Players.GetByRoomId(parseInt(id));
     let vowels = "aeiou";
     let consonants = "bcdfghjklmnpqrstvwxyz";
